fe107c1fc6a371c59e56124a87c58c4c7e3916de
hritik <86347286+hritikd3@users.noreply.github.com>
2022-01-13 11:46:05 +0530

    container updated

--- src/index.js
+++ src/index.js
@@ -5,20 +5,22 @@ const UseStateCounter = () => {
   const [value, setValue] = useState(0);
   return (
     <>
-      <h2> Random Counter </h2>
-      <h1> {value} </h1>
-      <button className="btn" onClick={() => setValue(value + 1)}>
-        {" "}
-        Increament{" "}
-      </button>
-      <button className="btn" onClick={() => setValue(0)}>
-        {" "}
-        Reset{" "}
-      </button>
-      <button className="btn" onClick={() => setValue(value - 1)}>
-        {" "}
-        Decreament{" "}
-      </button>
+      <div className="container">
+        <h2> Random Counter </h2>
+        <h1> {value} </h1>
+        <button className="btn" onClick={() => setValue(value + 1)}>
+          {" "}
+          Increament{" "}
+        </button>
+        <button className="btn" onClick={() => setValue(0)}>
+          {" "}
+          Reset{" "}
+        </button>
+        <button className="btn" onClick={() => setValue(value - 1)}>
+          {" "}
+          Decreament{" "}
+        </button>{" "}
+      </div>
     </>
   );
 };
